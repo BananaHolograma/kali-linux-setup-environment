7651382c705ad19088957a8267de16a4bd037bbc
0xp1n <s3r0s4pi3ns@protonmail.com>
2023-03-24 11:18:36 +0000

    new functions to setup nvm, blackarch

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-set -u
+set -eou pipefail
+
+# ANSII ESCAPE CODE COLOURS
+greenColour='\033[0;32m'
+redColour='\033[0;31m'
+blueColour='\033[0;34m'
+yellowColour='\033[1;33m'
+purpleColour='\033[0;35m'
+cyanColour='\033[0;36m'
+grayColour='\033[0;37m'
+
+endColour='\033[0m'
 
 CURRENT_DIR=$(pwd)
 
-source "$CURRENT_DIR/utils/colors.sh"
-
-# Auto detect the package manager for the target OS
 package_manager="sudo pacman"
-system_architecture=$(uname -m)
 
 # Common folders to work
 target_home_config_dir="$HOME/.config"
@@ -24,105 +31,82 @@ function backupTargetConfigurationFolder() {
     fi
 }
 
-function setupHotkeys() {
-    $package_manager install sxhkd
-    $package_manager install rofi
-
-    backupTargetConfigurationFolder
-
-    echo -e "${grayColour}Copying sxhkd configuration files in order to setup hotkeys...${endColour}"
-
-    cp -r "$CURRENT_DIR/config/sxhkd" "$target_home_config_dir"
-
-    echo -e "${greenColour} Hotkeys installed and configured with${endColour} ${cyanColour}[ sxhkd ]${endColour}"
-}
-
 function setupCustomTerminalFont() {
     echo -e "${grayColour}Downloading HackNerdFont from${endColour} ${blueColour}https://github.com/ryanoasis/nerd-fonts${endColour}"
 
     local fonts_dir="$HOME/.fonts"
     mkdir -p "$fonts_dir"
-    curl -sLo Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip && unzip -oq Hack.zip -d "$fonts_dir" && rm Hack.zip
+     
+    if curl -sLo Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip; then 
+        unzip -oq Hack.zip -d "$fonts_dir" && rm Hack.zip
+    else 
+        cp "$CURRENT_DIR/config/fonts/HackNerdFont/*" "$fonts_dir"
+    fi 
 
     echo -e "${greenColour} Fonts installed and configured in${endColour} ${cyanColour}[ $fonts_dir ]${endColour}"
 }
 
-function installTerminatorMultiplexor() {
-    $package_manager install terminator
-}
 
 function setupAndConfigureKitty() {
     echo -e "${grayColour}Installing and configuring kitty GPU based terminal...${endColour}"
 
-    local kitty_dir=/opt/kitty
-    local base_url=https://github.com/kovidgoyal/kitty/releases/download/v0.27.1
-    local kitty_release=''
-
-    case $system_architecture in
-        arm64 | aarch64)
-            kitty_release="kitty-0.27.1-arm64.txz"
-        ;;
-        64-bit | x86_64)
-            kitty_release="kitty-0.27.1-x86_64.txz"
-        ;;
-        i386| i486| i586| i686)
-        kitty_release="kitty-0.27.1-i686.txz"
-        ;;
-    esac
-
-    curl -sLo kitty.txz "$base_url/$kitty_release"
-    sudo mkdir -p $kitty_dir && sudo tar -xf kitty.txz -C $kitty_dir && rm kitty.txz
-    sudo ln -sf $kitty_dir/bin/kitty /usr/local/bin/kitty
-
-    cp -r "$CURRENT_DIR/config/kitty" "$target_home_config_dir"
+    "$package_manager" -S kitty \
+        && cp -r "$CURRENT_DIR/config/kitty" "$target_home_config_dir"
 
     echo -e "${greenColour} Kitty GPU based terminal installed and configured on${endColour} ${cyanColour}[ $(which kitty) ]${endColour}"
 }
 
 function setupZSH() {
     echo -e "${grayColour} Installing and configuring zsh terminal with powerlevel10k theme${endColour}"
+    local ZSH_CONFIG_DIR="$HOME/.config/zsh"
 
     if [ -f "$HOME"/.zshrc ]; then
         echo -e "${greenColour}Detected existing .zshrc file${endColour}, ${yellowColour}creating backup on${endColour} ${cyanColour}$config_backup_folder"
         cp "$HOME"/.zshrc "$config_backup_folder"
     fi
 
-    mkdir -p "$HOME/.config/zsh/plugins"
-    touch "$HOME.config/zsh/.zsh_history"
+    "$package_manager" -S base-devel git zsh
 
-    zsh 
+    mkdir -p "$ZSH_CONFIG_DIR/plugins"
+    touch "$ZSH_CONFIG_DIR/.zsh_history"
+
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CONFIG_DIR/plugins"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  "$ZSH_CONFIG_DIR/plugins"
+    cp "$CURRENT_DIR/config/zsh/plugins/colored-man-pages/*" "$ZSH_CONFIG_DIR/plugins"
+
+    zsh
 }
 
 function setupNVM() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 }
 
+function setupBlackArchRepository() {
+    curl -O https://blackarch.org/strap.sh
+
+    chmod +x && sudo bash strap.sh
+
+    "$package_manager" -Syu
+}
+
+function setupFirejail() {
+    "$package_manager"-Sy firejail 
+
+    # We installed yay to get access firefox binaries
+    git clone https://aur.archlinux.org/yay-git.git
+
+    cd yay-git && makepkg -sri
+
+    # firefox: Stable regular build
+    # firefox-beta: Stable pre-release build
+    # firefox-nightly: Unstable and testing build
+    $ yay -S firefox
+}
+
 function setupTerminalUtils() {
-    # batcat
-    $package_manager install bat && mkdir -p ~/.local/bin && ln -sf /usr/bin/batcat ~/.local/bin/bat
-    # fzf
-    $package_manager install fzf
-
-    #lsd
-    if [ "$package_manager install" = 'sudo apt' ]; then
-        local lsd_release=''
-
-        case $system_architecture in
-        arm64 | aarch64)
-            lsd_release="lsd_0.23.1_arm64.deb"
-            ;;
-        64-bit | x86_64)
-            lsd_release="lsd_0.23.1_amd64.deb"
-            ;;
-        i386| i486| i586| i686)
-        lsd_release="lsd_0.23.1_i686.deb"
-            ;;
-         esac
-        curl -sLo lsd_release.deb https://github.com/Peltoche/lsd/releases/download/0.23.1/$lsd_release
-        sudo dpkg -i lsd_release.deb
-    else
-        $package_manager install lsd
-    fi
+    # batcat, lsd, fzf
+    $package_manager -S bat fzf lsd \
+        && mkdir -p ~/.local/bin && ln -sf /usr/bin/batcat ~/.local/bin/bat
 }
 
 ###
@@ -131,4 +115,7 @@ function setupTerminalUtils() {
 setupCustomTerminalFont
 setupAndConfigureKitty
 setupTerminalUtils
+setupBlackArchRepository
+setupFirejail
+setupNVM
 setupZSH
