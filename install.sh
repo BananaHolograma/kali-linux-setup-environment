968df9237c9f6b6045d1b41b804da6d2d07493a7
0xp1n <s3r0s4pi3ns@protonmail.com>
2023-05-06 12:42:43 +0100

    implemented function stepNeedInstallation to avoid repetitive tasks

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ while ! user_exists "$SELECTED_USER" && [ "$create_non_existing_user" = 'n' ]; d
 
     while [[ -z $SUDO_PASSWORD ]]; do 
         echo -n "Write the sudo password for your user $SELECTED_USER to install packages with privileges: " 
-        read -r SUDO_PASSWORD
+        read -rs SUDO_PASSWORD
     done 
 done
 
@@ -63,36 +63,53 @@ ROOT_DIR="/root"
 
 
 # Common folders to work
-target_home_config_dir="$HOME_DIR/.config"
-config_backup_folder="$HOME_DIR/backup/${SELECTED_USER}.config"
+TARGET_HOME_CONFIG_DIR="$HOME_DIR/.config"
+CONFIG_BACKUP_FOLDER="$HOME_DIR/backup/${SELECTED_USER}.config"
+
+function stepNeedInstallation() {
+    local tmp_folder="/tmp/custom_kali_installation"
+    local step=$1
+
+    mkdir -p "$tmp_folder"
+
+    if [[ ! -f "$tmp_folder/$step" ]]; then 
+        touch "$tmp_folder/$step"
+        return 1
+    else 
+        echo -e "${greenColour}[ INSTALLED ]$endColour${yellowColour} The step $step is already installed and configured, skipping...$endColour"
+        return 0
+    fi 
+
+}
 
 function prepareEnvironmentForTheInstallation() {
-    if [ -d "$target_home_config_dir" ]; then
-        echo -e "${grayColour}[ PREPARATION ]$endColour ${yellowColour}Detected existing$endColour$cyanColour .config$endColour$yellowColour folder, creating backup on$endColour$cyanColour $config_backup_folder$endColour"
+    if [ -d "$TARGET_HOME_CONFIG_DIR" ]; then
+        echo -e "${cyanColour}[ PREPARATION ]$endColour ${yellowColour}Detected existing$endColour$cyanColour .config$endColour$yellowColour folder, creating backup on$endColour$cyanColour $CONFIG_BACKUP_FOLDER$endColour"
 
-        mkdir -p "$config_backup_folder" \
-            && cp -r "$target_home_config_dir" "$config_backup_folder"
+        mkdir -p "$CONFIG_BACKUP_FOLDER" \
+            && cp -r "$TARGET_HOME_CONFIG_DIR" "$CONFIG_BACKUP_FOLDER"
     fi
 
-    echo -e "${grayColour}[ PREPARATION ]$endColour$yellowColour Installing packages that are needed in the system to continue the process...$endColour"
+    echo -e "${cyanColour}[ PREPARATION ]$endColour$yellowColour Installing and updating packages that are needed in the system to continue the process...$endColour"
     
     # We only need to provide the sudo password one time at the start of the script
     echo "$SUDO_PASSWORD" | sudo -S apt update
 
-    sudo apt upgrade -y && sudo apt install -y grc jq net-tools iputils-ping socat cifs-utils tldr awscli docker.io docker-compose rsync parallel mongodb-clients freerdp2-x11
+    sudo apt upgrade -yqq && sudo apt install -yqq -o=Dpkg::Use-Pty=0  grc jq net-tools iputils-ping socat cifs-utils tldr awscli docker.io docker-compose rsync parallel mongodb-clients freerdp2-x11
+
 }
 
 function setupCustomTerminalFont() {
     local fonts_dir="$HOME_DIR/.fonts"
 
     if [[ -f "$fonts_dir"/Hack\ Regular\ Nerd\ Font\ Complete.ttf ]]; then
-        echo -e "${grayColour}[ FONTS ]$endColour$yellowColour HackNerdFont font is already installed in the system, skipping...${endColour}"
+        echo -e "${cyanColour}[ FONTS ]$endColour$yellowColour HackNerdFont font is already installed in the system, skipping...${endColour}"
     else
-        echo -e "${grayColour}[ FONTS ]$endColour$yellowColour Downloading HackNerdFont from$endColour$yellowColour https://github.com/ryanoasis/nerd-fonts$endColour"
+        echo -e "${cyanColour}[ FONTS ]$endColour$yellowColour Downloading HackNerdFont from$endColour$yellowColour https://github.com/ryanoasis/nerd-fonts$endColour"
 
         mkdir -p "$fonts_dir"
         
-        if curl -sLo Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip; then 
+        if curl --fail -sLo Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip; then 
             unzip -oq Hack.zip -d "$fonts_dir" && rm Hack.zip
         else 
              find "$CURRENT_DIR/config/fonts/HackNerdFont" -type f -execdir cp -t "$fonts_dir" {} +
@@ -103,40 +120,40 @@ function setupCustomTerminalFont() {
 }
 
 function setupAndConfigureKitty() {
-    echo -e "${grayColour}[ KITTY ]$endColour$yellowColour Installing and configuring kitty GPU based terminal...$endColour"
+    echo -e "${cyanColour}[ KITTY ]$endColour$yellowColour Installing and configuring kitty GPU based terminal...$endColour"
 
-    sudo apt install -y kitty \
-        && cp -r "$CURRENT_DIR/config/kitty" "$target_home_config_dir"
+    sudo apt install -yqq -o=Dpkg::Use-Pty=0 kitty \
+        && cp -r "$CURRENT_DIR/config/kitty" "$TARGET_HOME_CONFIG_DIR"
 
-    echo -e "${grayColour}[ KITTY ]$endColour$yellowColour Kitty GPU based terminal installed and configured on$endColour$cyanColour [ $(which kitty) ]$endColour"
+    echo -e "${cyanColour}[ KITTY ]$endColour$yellowColour Kitty GPU based terminal installed and configured on$endColour$cyanColour [ $(which kitty) ]$endColour"
 }
 
 function setupVim() {
-    echo -e "${grayColour}[ VIM ]$endColour Installing and configuring VIM editor with basic initial configuration"
-    local VIM_CONFIG_DIR="$CURRENT_DIR/config/vim/"
+    echo -e "${cyanColour}[ VIM ]$endColour Installing and configuring VIM editor with basic initial configuration"
+    local VIM_CONFIG_DIR="$CURRENT_DIR/config/vim"
     
-    sudo apt install -y vim
+    sudo apt install --yqq -o=Dpkg::Use-Pty=0 vim
 
     if [ -f "$HOME_DIR"/.vimrc ]; then
-        echo -e "${grayColour}[ VIM ]$endColour$yellowColour Detected existing .vimrc file, creating backup on$endColour$cyanColour $config_backup_folder"
-        cp "$HOME_DIR"/.vimrc "$config_backup_folder"
+        echo -e "${grayColour}[ VIM ]$endColour$yellowColour Detected existing .vimrc file, creating backup on$endColour$cyanColour $CONFIG_BACKUP_FOLDER"
+        cp "$HOME_DIR"/.vimrc "$CONFIG_BACKUP_FOLDER"
     fi
 
     [[ -f "$VIM_CONFIG_DIR/.vimrc" ]] && cp -f "$VIM_CONFIG_DIR/.vimrc" "$HOME_DIR"
 
-    echo -e "${grayColour}[ VIM ]$endColour$yellowColour Created$endColour$cyanColour .vimrc$endColour$yellowColour file on $HOME_DIR directory$endColour"
+    echo -e "${cyanColour}[ VIM ]$endColour$yellowColour Created$endColour$cyanColour .vimrc$endColour$yellowColour file on $HOME_DIR directory$endColour"
 }
 
 function setupZSH() {
-    echo -e "${grayColour}[ ZSH ]$endColour$yellowColour Installing and configuring zsh$endColour"
+    echo -e "${cyanColour}[ ZSH ]$endColour$yellowColour Installing and configuring zsh$endColour"
 
     local ZSH_CONFIG_DIR="$HOME_DIR/.config/zsh"
 
-    sudo apt install -y zsh
+    sudo apt install ---yqq -o=Dpkg::Use-Pty=0 zsh
 
     if [ -f "$HOME_DIR"/.zshrc ]; then
-        echo -e "${grayColour}[ ZSH ]$endColour$yellowColour Detected existing .zshrc file, creating backup on$endColour$cyanColour $config_backup_folder"
-        cp "$HOME_DIR"/.zshrc "$config_backup_folder"
+        echo -e "${grayColour}[ ZSH ]$endColour$yellowColour Detected existing .zshrc file, creating backup on$endColour$cyanColour $CONFIG_BACKUP_FOLDER"
+        cp "$HOME_DIR"/.zshrc "$CONFIG_BACKUP_FOLDER"
     fi
 
     mkdir -p "$ZSH_CONFIG_DIR/plugins" 
@@ -160,8 +177,10 @@ function setupZSH() {
 }
 
 function setupInfoSecTools() {
+    echo -e "${cyanColour}[ INFOSEC TOOLS ]$endColour${yellowColour} Installing infosec tools...$endColour"
+   
     sudo apt remove python3-httpx subfinder && sudo apt autoremove --purge
-    sudo apt install -y firejail python3 python3-pip xxd ghidra tor sqlmap dnsrecon wafw00f burpsuite whois amass massdns golang-go masscan nmap brutespray ffuf exploitdb openjdk-11-jdk maven
+    sudo apt install ---yqq -o=Dpkg::Use-Pty=0 firejail python3 python3-pip xxd ghidra tor sqlmap dnsrecon wafw00f burpsuite whois amass massdns golang-go masscan nmap brutespray ffuf exploitdb openjdk-11-jdk maven
     
     if [[ ! -d "/usr/share/SecLists" ]]; then 
         wget -c -nc https://github.com/danielmiessler/SecLists/archive/master.zip -O SecList.zip \
@@ -170,14 +189,18 @@ function setupInfoSecTools() {
             && sudo rm -f SecList.zip
     fi 
     
+    echo -e "${cyanColour}[ INFOSEC TOOLS ]$endColour${yellowColour} Preparing wordlists...$endColour"
+
     if [[ ! -d "/usr/share/wordlists"]]; then 
         sudo apt install wordlists
     fi 
     
     if [[ ! -f "/usr/share/wordlists/rockyou.txt" ]]; then
-        echo -e "${grayColour}[ WORDLISTS ]$endColour$yellowColour Extracting rockyou.txt.gz ... $endColour"
+        echo -e "${cyanColour}[ INFOSEC TOOLS ]$endColour${yellowColour} Extracting rockyou.txt.gz ... $endColour"
         sudo gunzip /usr/share/wordlists/rockyou.txt.gz
     fi 
+
+    echo -e "${cyanColour}[ INFOSEC TOOLS ]$endColour${yellowColour} Downloading and configuring custom bash libraries... $endColour"
 
     libraries=(crt randomipzer ipharvest) 
     for library in "${libraries[@]}"
@@ -195,15 +218,15 @@ function setupInfoSecTools() {
 
     if [[ ! -d "$HOME_DIR"/jwt_sec_tool ]]; then 
         JWT_TOOL_DIR="$HOME_DIR"/jwt_sec_tool/jwt_tool.py
-        python3 -m pip install termcolor cprint pycryptodomex requests
 
+        python3 -m pip install termcolor cprint pycryptodomex requests
         git clone https://github.com/ticarpi/jwt_tool jwt_sec_tool
-        chmod +x "$JWT_TOOL_DIR" && cp "$JWT_TOOL_DIR" /usr/local/bin/jwt_tool
+        chmod +x "$JWT_TOOL_DIR" && sudo cp "$JWT_TOOL_DIR" /usr/local/bin/jwt_tool
     fi 
 
     # GO binary path is exported on .zshrc
     if command_exists 'go'; then 
-        echo -e "${cyanColour}[ GOLANG ]$endColour$yellowColour Installing golang security tools ... $endColour"
+        echo -e "${cyanColour}[ INFOSEC TOOLS ]$endColour$yellowColour Installing golang security tools ... $endColour"
 
         ! command_exists 'hakrawler' && go install github.com/hakluke/hakrawler@latest 
         ! command_exists 'gau' && go install github.com/lc/gau/v2/cmd/gau@latest
@@ -214,6 +237,8 @@ function setupInfoSecTools() {
         ! command_exists 'dnsx' && go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest
         
         if ! command_exists 'puredns' && command_exists 'massdns'; then 
+            echo -e "${cyanColour}[ INFOSEC TOOLS ]$endColour$yellowColour Setup valid DNS resolvers... $endColour"
+
             go install github.com/d3mondev/puredns/v2@latest
 
             if [[ ! -d "$HOME_DIR/dns-resolvers" ]]; then 
@@ -226,14 +251,14 @@ function setupInfoSecTools() {
 }
 
 function setupTerminalUtils() {
-    echo -e "${grayColour}[ TERMINAL UTILS ]$endColour ${yellowColour}Installing and configuring terminal utils...$endColour"
+    echo -e "${cyanColour}[ TERMINAL UTILS ]$endColour ${yellowColour}Installing and configuring terminal utils...$endColour"
     
     sudo apt install -y bat fzf lsd bash-completion \
         && mkdir -p ~/.local/bin && ln -sf /usr/bin/batcat ~/.local/bin/bat
 }
 
 function setupScripts() {
-        echo -e "${grayColour}[ TERMINAL UTILS ]$endColour ${yellowColour}Copying custom scripts into$endColour ${cyanColour}$HOME_DIR/scripts..$endColour"
+        echo -e "${cyanColour}[ CUSTOM SCRIPTS ]$endColour ${yellowColour}Copying custom scripts into$endColour ${cyanColour}$HOME_DIR/scripts..$endColour"
 
         mkdir -p "$HOME_DIR/scripts" \
             && cp -rf "$CURRENT_DIR/scripts" "$HOME_DIR/scripts"
@@ -242,14 +267,14 @@ function setupScripts() {
 ###
 # START THE INSTALLATION AND CONFIGURATION PROCESS FOR THE NEW ENVIRONMENT
 ###
-prepareEnvironmentForTheInstallation
-setupCustomTerminalFont
-setupAndConfigureKitty
-setupVim
-setupZSH
-setupTerminalUtils
-setupScripts
-setupInfoSecTools
+stepNeedInstallation "PREPARE_ENVIRONMENT" && prepareEnvironmentForTheInstallation
+stepNeedInstallation "TERMINAL_FONT" && setupCustomTerminalFont
+stepNeedInstallation "KITTY_GPU_TERMINAL" && setupAndConfigureKitty
+stepNeedInstallation "VIM" && setupVim
+stepNeedInstallation "ZSH" && setupZSH
+stepNeedInstallation "TERMINAL_UTILS" && setupTerminalUtils
+stepNeedInstallation "CUSTOM_SCRIPTS" && setupScripts
+stepNeedInstallation "INFOSEC_TOOLS" && setupInfoSecTools
 
 # Copy the entire configuration to root home folder in order to have same configuration
 sudo cp -rf "$HOME_DIR"/.config "$ROOT_DIR"
